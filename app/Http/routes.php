4a4469733070344f44a265de9a66ec3737bd7efd
Baldomero Águila Napoli <baldomer.napoli@gmail.com>
2016-01-25 12:35:07 -0300

    Se modificó la nomenclatura de rutas para que se puedan agregar acciones personalizadas en los controller

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -20,13 +20,37 @@ Route::controllers([
 	'password' => 'Auth\PasswordController',
 ]);
 
-Route::resource('continentes','ContinentesController');
-Route::resource('paises','PaisesController');
-
-
 Route::group(['prefix'=>'admin'],function(){
 
 	//Route::resource('users','UsersController');
 	Route::resource('usuarios','UsuariosController');
+
+});
+
+
+Route::group(['prefix'=>'continentes'],function(){
+
+	Route::get('/',array('uses' => 'ContinentesController@index', 'as'=>'continentes.index'));
+	Route::get('create',array('uses' => 'ContinentesController@create', 'as'=>'continentes.create'));
+	Route::post('store',array('uses' => 'ContinentesController@store', 'as'=>'continentes.store'));
+	Route::get('edit/{id}',array('uses' => 'ContinentesController@edit', 'as' => 'continentes.edit'));
+	Route::put('update/{id}',array('uses' => 'ContinentesController@update', 'as' => 'continentes.update'));
+	Route::delete('destroy({id}',array('uses' => 'ContinentesController@destroy', 'as'=> 'continentes.destroy'));
+
+
+
+});
+
+
+Route::group(['prefix'=>'paises'],function(){
+
+	Route::get('/',array('uses' => 'PaisesController@index', 'as'=>'paises.index'));
+	Route::get('create',array('uses' => 'PaisesController@create', 'as'=>'paises.create'));
+	Route::post('store',array('uses' => 'PaisesController@store', 'as'=>'paises.store'));
+	Route::get('edit/{id}',array('uses' => 'PaisesController@edit', 'as' => 'paises.edit'));
+	Route::put('update/{id}',array('uses' => 'PaisesController@update', 'as' => 'paises.update'));
+	Route::delete('destroy({id}',array('uses' => 'PaisesController@destroy', 'as'=> 'paises.destroy'));
+
+
 
 });

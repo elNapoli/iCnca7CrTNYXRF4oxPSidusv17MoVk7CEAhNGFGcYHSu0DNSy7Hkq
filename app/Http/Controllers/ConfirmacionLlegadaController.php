00f0fe0bc587506507ca40c0750eebe0fe5d96ab
napoli <napoli>
2016-02-21 14:49:06 -0300

    confirmación de llegada 100% listo

--- app/Http/Controllers/ConfirmacionLlegadaController.php
+++ app/Http/Controllers/ConfirmacionLlegadaController.php
@@ -4,6 +4,7 @@ use Illuminate\Contracts\Auth\Guard;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use App\PrePostulacionUniversidad;
+use App\ConfirmacionLlegada;
 use App\Postulante;
 use Illuminate\Http\Request;
 
@@ -14,15 +15,34 @@ class ConfirmacionLlegadaController extends Controller {
 		$postulante = Postulante::where('user_id',$auth->id())->first();
 		$postulacion_universidad = PrePostulacionUniversidad::where('postulante',$postulante->id)->first();
 
-		$parametros = array(
+		$parametros = array(	
+								'fecha_llegada'=>'',
+								'fecha_inicio_curso'=>'',
+								'fecha_termino_curso'=>'',
+								'postulante' => $postulante->id,
 								'nombre_universidad_destino' => $postulacion_universidad->carreraR->facultadR->campusSedesR->universidadR->nombre,
 								'nombre_coordinador' => $postulacion_universidad->carreraR->facultadR->campusSedesR->departamentosR()->first()->nombre_encargado,
 							    "nombre_estudiante" => $postulante->nombre.' '. $postulante->apellido_paterno.' '.$postulante->apellido_materno,
 								'asignaturas_homologadas' => $postulante->pregradosR->preUachsR->homologacionesR()->first()->asignaturaHomologadaR->toArray(),
 							);
+		$confirmacion = ConfirmacionLlegada::where('postulante',$postulante->id);
+		if($confirmacion->get()->count() != 0){
 
-		
+			$parametros['fecha_llegada']       = $confirmacion->first()->fecha_llegada;
+			$parametros['fecha_inicio_curso']  = $confirmacion->first()->fecha_inicio_curso;
+			$parametros['fecha_termino_curso'] = $confirmacion->first()->fecha_termino_curso;
+		}
 		return view('confirmacionLlegada.index',compact('parametros'));
+	}
+
+	public function postStoreAndUpdate(Request $Request){
+		
+		$confirmacion = ConfirmacionLlegada::firstOrNew(['postulante' => $Request->get('postulante')]);
+		$confirmacion->fill($Request->all());
+		$confirmacion->save();
+		return response()->json([
+				'message'=> 'sus datos de confirmación de llegada se han almacenado correctamente.'
+				]);
 	}
 
 }
---
+++ resources/views/confirmacionLlegada/partials/fields.blade.php
@@ -0,0 +1,79 @@
+   <div class="form-horizontal">
+        	<div class="col-lg-6">
+        		<h4>Datos de Universidad de Destino</h4>
+	          	<div class="form-group">
+	            	{!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
+
+		            <div class="col-lg-6">
+			            {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
+
+		            </div>
+	          	</div>
+
+	          	<div class="form-group">
+	            	{!!  Form::label('nombre_coordinador', 'Coordinador:',array('class'=>'col-lg-5 control-label'));!!}
+
+		            <div class="col-lg-6">
+			            {!! Form::text('nombre_coordinador',null,array('class' => 'form-control','disabled'=>''));!!}
+
+		            </div>
+	          	</div>
+	          	<h4>Datos personales del estudiante</h4>
+	          	<div class="form-group">
+	            	{!!  Form::label('nombre_estudiante', 'Nombre completo: ',array('class'=>'col-lg-5 control-label'));!!}
+
+		            <div class="col-lg-6">
+			            {!! Form::text('nombre_estudiante',null,array('class' => 'form-control','disabled'=>''));!!}
+
+		            </div>
+	          	</div>
+	          	
+              <div class="panel panel-green">
+                        <div class="panel-heading">
+                        <h5>Cursos en la universidad de destino</h5>
+                        </div>
+                        <div class="panel-body">
+                          <ul>
+                             @foreach ($parametros['asignaturas_homologadas'] as $asignatura)
+
+                                <li><strong>{{$asignatura['codigo_asignatura_intercambio'] .':    '}}</strong>{{$asignatura['nombre_asignatura_intercambio']}}</li>
+                             @endforeach
+                          </ul>
+                        </div>
+                   
+                    </div>
+        	</div>
+        	<div class="col-lg-6">
+        		<h4>Confirmación de Arribo y Registro</h4>
+          	<div class="form-group">
+            	{!!  Form::label('fecha_llegada', 'Fecha de presentación en Institución de 
+				        Destino: ',array('class'=>'col-lg-5 control-label'));!!}
+
+	            <div class="col-lg-6">
+		            {!! Form::text('fecha_llegada',null,array('class' => 'form-control'));!!}
+
+	            </div>
+          	</div>
+          	<div class="form-group">
+            	{!!  Form::label('fecha_inicio_curso', 'Fecha inicio de cursos: ',array('class'=>'col-lg-5 control-label'));!!}
+
+	            <div class="col-lg-6">
+		            {!! Form::text('fecha_inicio_curso',null,array('class' => 'form-control'));!!}
+
+	            </div>
+          	</div>
+          	<div class="form-group">
+            	{!!  Form::label('fecha_termino_curso', 'Fechas término de cursos: ',array('class'=>'col-lg-5 control-label'));!!}
+
+	            <div class="col-lg-6">
+		            {!! Form::text('fecha_termino_curso',null,array('class' => 'form-control'));!!}
+
+	            </div>
+          	</div>
+            <a class="btn-info btn" id='guardarConfirmacion' href="#!">Guardar</a>
+        	</div>
+			
+          	
+{!!Form::hidden('postulante', null,array('id'=>'postulante'));!!}
+
+        </div>

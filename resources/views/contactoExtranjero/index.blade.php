7c6bf868179421bce4797a5808ffc144224aab97
napoli <napoli>
2016-02-22 13:41:46 -0300

    contacto extrajero UACH 100% listo

--- resources/views/contactoExtranjero/index.blade.php
+++ resources/views/contactoExtranjero/index.blade.php
@@ -3,7 +3,12 @@
 @section('Dashboard') Postulación @endsection
 
 @section('content')
-	@include('contactoExtranjero.partials.fields')
+  {!! Form::model($parametros, ['url'=>['contacto-en-extranjero/store-and-update'], 'method'=>'post','id'=>'form-save-contacto-extranjero']) !!}
+      		<div class="message"></div>
+             
+			@include('contactoExtranjero.partials.fields')
+  {!!Form::close()!!}
+  
 @endsection
 
 @section('breadcrumbs')
@@ -13,7 +18,46 @@
 	<script>
 		 $(document).on('ready',function(){
 
+ 				$('#validez_seguro').datepicker({
 
+                    showButtonPanel: true,
+                    changeMonth: true,
+                    changeYear: true,
+                    dateFormat: 'yy-mm-dd',
+                    showAnim: 'drop',
+
+                });
+		 	$('#guardarContactoExtranjero').on('click',function(){
+                data = $('#form-save-contacto-extranjero').serialize();
+             	$.ajax({
+                                  
+	                async : false,
+	                data:data,
+	                //Cambiar a type: POST si necesario
+	                type: 'POST',
+	                // Formato de datos que se espera en la respuesta
+	                dataType: "json",
+	                // URL a la que se enviará la solicitud Ajax
+	                url:$('#form-save-contacto-extranjero').attr('action') ,
+	                success : function(json) {   
+	                    $('.message').html('<div class="alert alert-success fade in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button>'+json.message+'</div>');  
+	             
+	                    
+	                },
+
+	                error : function(xhr, status) {
+	                     var html = '<div class="alert alert-danger fade in"><button type="button" class="close close-alert" data-dismiss="alert" aria-hidden="true">×</button><p> Porfavor corregir los siguientes errores:</p>';
+	                        for(var key in xhr.responseJSON)
+	                        {
+	                            html += "<li>" + xhr.responseJSON[key][0] + "</li>";
+	                        }
+	                        $('.message').html(html+'</div>');
+	              
+	                },
+	                
+
+	            });
+		 	});
 		 });
 	</script>
 @endsection
--- resources/views/contactoExtranjero/partials/fields.blade.php
+++ resources/views/contactoExtranjero/partials/fields.blade.php
@@ -1,126 +1,152 @@
 <div class="form-horizontal">
-    <div class="form-group">
-      {!!  Form::label('nombre_estudiante', 'Nombre del estudiante: ',array('class'=>'col-lg-5 control-label'));!!}
-
       <div class="col-lg-6">
-        {!! Form::text('nombre_estudiante',null,array('class' => 'form-control','disabled'=>''));!!}
+            <div class="form-group">
+              {!!  Form::label('nombre_estudiante', 'Nombre del estudiante: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('nombre_estudiante',null,array('class' => 'form-control','disabled'=>''));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('n_pasaporte', 'Número de pasaporte: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('n_pasaporte',null,array('class' => 'form-control','disabled'=>''));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('direccion', 'Dirección en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('direccion',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('ciudad_pais', 'Ciudad y país en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('ciudad_pais',null,array('class' => 'form-control','disabled'=>''));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('telefono_1', 'Teléfono en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('telefono_1',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('telefono_2', 'Celular en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('telefono_2',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('conocido_extranjero', 'Contacto de algún conocido en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('conocido_extranjero',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('nombre_seguro', 'Nombre del seguro internacional: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('nombre_seguro',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('numero_seguro', 'Número del seuro internacional: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('numero_seguro',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
 
       </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('n_pasaporte', 'Número de pasaporte: ',array('class'=>'col-lg-5 control-label'));!!}
 
       <div class="col-lg-6">
-        {!! Form::text('n_pasaporte',null,array('class' => 'form-control','disabled'=>''));!!}
+
+
+
+            
+
+            <div class="form-group">
+              {!!  Form::label('validez_seguro', 'Validez exacta del seguro internacional: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('validez_seguro',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('nombre_hospital', 'Nombre del Hospital más cercano a tu lugar de residencia:',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('nombre_hospital',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('direccion_hospital', 'dirección Hospital más cercano a tu lugar de residencia: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('direccion_hospital',null,array('class' => 'form-control'));!!}
+
+              </div>
+            </div>
+
+            <div class="form-group">
+              {!!  Form::label('nombre_contacto', 'Nombre de algún familiar en chile para casos de emergencia: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('nombre_contacto',null,array('class' => 'form-control','disabled'=>''));!!}
+
+              </div>
+            </div>
+            <div class="form-group">
+              {!!  Form::label('tel_contacto', 'Teléfono/Celular del contacto en Chile: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('tel_contacto',null,array('class' => 'form-control','disabled'=>''));!!}
+
+              </div>
+            </div>
+            <div class="form-group">
+              {!!  Form::label('parentesco_contacto', 'Parentesco del contacto en Chile: ',array('class'=>'col-lg-5 control-label'));!!}
+
+              <div class="col-lg-6">
+                {!! Form::text('parentesco_contacto',null,array('class' => 'form-control','disabled'=>''));!!}
+
+              </div>
+            </div>
+        <a class="btn-info btn" id='guardarContactoExtranjero' href="#!">Guardar</a>
+    {!!Form::hidden('postulante', null,array('id'=>'postulante'));!!}
 
       </div>
-    </div>
 
-    <div class="form-group">
-      {!!  Form::label('dir_extranjero', 'Dirección en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
 
-      <div class="col-lg-6">
-        {!! Form::text('dir_extranjero',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('ciudad_pais', 'Ciudad y país en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('ciudad_pais',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('telefono_extranjero', 'Teléfono en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('telefono_extranjero',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('celular_extranjero', 'Celular en el extranjero: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('celular_extranjero',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
-    <div class="form-group">
-      {!!  Form::label('nombre_universidad_destino', 'Nombre de la institución: ',array('class'=>'col-lg-5 control-label'));!!}
-
-      <div class="col-lg-6">
-        {!! Form::text('nombre_universidad_destino',null,array('class' => 'form-control','disabled'=>''));!!}
-
-      </div>
-    </div>
+ 
  
 
 

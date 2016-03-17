5290b48d9999570a94d2e543c93da7ca09b10b27
napoli <napoli>
2016-03-16 22:14:40 -0300

    respaldo

--- resources/views/documentospostulacion/uploadFile.blade.php
+++ resources/views/documentospostulacion/uploadFile.blade.php
@@ -3,7 +3,18 @@
 @section('Dashboard') Departamentos @endsection
 
 @section('content')
+<!-- BASIC FORM ELELEMNTS -->
+            <div class="row mt">
+                <div class="col-lg-12">
+                  <div class="form-panel">
+                      <h4 class="mb"><i class="fa fa-angle-right"></i> Porfavor adjuntar los siguientes documentos</h4>
+                      <form class="form-horizontal style-form" method="get">
+                         
 <input id="input-706" name="documentosAdjuntos[]" type="file" multiple=true class="file-loading">
+                      </form>
+                  </div>
+                </div><!-- col-lg-12-->         
+            </div><!-- /row -->
  
 {!!Form::hidden('_token', csrf_token(),array('id'=>'_token'));!!}
 {!!Form::hidden('getUrlFiles', url('docs/all-files'),array('id'=>'getUrlFiles'));!!}

2facc2b9125836c1e1bd1d3c050448bd6cad8b8e
napoli <napoli>
2016-04-03 01:40:27 -0300

    adf

---
+++ resources/views/documentospostulacion/partials/table_doc.blade.php
@@ -0,0 +1,64 @@
+<table id="todo" class="table table-hover custom-check">
+    <tbody>
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="#!">Formulario de postulación completo con fotofrafía.</a></span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked" checked></span>
+                <a href="index.html#">Concentración de notas para uso interno.</a></span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Cerfiticado de competencias lengüísticas en caso de ser necesario.</a></span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Currículum Vitae. </a></span>
+        </tr>
+
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Ranking académico. </a></span>
+        </tr>
+
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Certificado de alumno regular. </a></span>
+        </tr>
+
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Programa de estudios a realizar en la universidad extranjera firmada por la escuela (Homologación). </a></span>
+        </tr>
+
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Declaración jugara sobre financiamiento de la estadía en el extranjero (en caso de no tener beca). </a></span>
+        </tr>
+
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Certificado de salud compatible con el intercambio. </a></span>
+        </tr>
+
+        <tr>
+            <td>
+                <span class="check"><input type="checkbox" class="checked"></span>
+                <a href="index.html#">Carta de aceptación de la universidad de destino. </a></span>
+        </tr>
+
+    </tbody>
+</table>

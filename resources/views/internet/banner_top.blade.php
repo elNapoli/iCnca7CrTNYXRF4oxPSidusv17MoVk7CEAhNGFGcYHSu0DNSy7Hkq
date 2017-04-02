595c705ac627e726befefbd70cd9e2d01facde12
El_Napoli <baldomero.napoli@gmail.com>
2017-04-02 12:19:38 -0300

    add DropdownList to choose language

--- resources/views/internet/banner_top.blade.php
+++ resources/views/internet/banner_top.blade.php
@@ -1,10 +1,22 @@
 <div id="top">
     <div id="menu_1">
-        <p><a href="/inicio-uach/contacto">Contacto</a>&nbsp;| 
-            <a href="/inicio-uach/mapa">Mapa del Sitio</a> |&nbsp;
-            <a href="http://intranet.uach.cl">Intranet</a>
-        </p>    
+        <a href="/inicio-uach/contacto">Contacto</a>&nbsp;| 
+        <a href="/inicio-uach/mapa">Mapa del Sitio</a> |&nbsp;
+        <a href="http://intranet.uach.cl">Intranet</a>
+        <li class="dropdown lang">
+   				<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
+         			<span class="flag es">  </span>
+                 	<b class="caret"></b>
+       			</a>
+          		<ul class="dropdown-menu menu-lang">
+          			<li>
+          				<a href="#" hreflang="fr"><span class="flag en"></span></a>
+          			</li>
+      			</ul>
+    	</li>
+
     </div>
+
 </div>
 
 <div id="barra_top">

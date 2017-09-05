978fe119f9af01019092ebab9a37796ee31b7be9
Hellthrash <hellthrash@gmail.com>
2017-09-05 13:08:36 -0300

    agregando prototipo agenda

--- resources/views/welcome/partials/noticias.blade.php
+++ resources/views/welcome/partials/noticias.blade.php
@@ -1,8 +1,91 @@
-<div class="panel panel-success">
-  <div class="panel-heading">Sobre el departamento</div>
-  <div class="panel-body scroll">
-   Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda facere, qui fugiat. Porro eum eos unde debitis magni ad labore totam, vel inventore sapiente ea! Possimus impedit accusantium blanditiis sequi!Eius delectus perferendis quibusdam omnis quod possimus consectetur eos, similique dolores. Odio quo eius quas quia, dolor in saepe id! Maiores accusantium consequuntur illo non cumque obcaecati perspiciatis, minus est.Harum laborum cupiditate eaque inventore laboriosam, alias, consectetur. Similique sapiente, ad perspiciatis soluta neque assumenda officiis quo? Voluptatum, quos, nobis! Iste, totam alias dignissimos qui? Asperiores voluptate accusamus sapiente voluptatibus?A, deserunt. Porro magni atque aperiam iure nemo id dolore, dolorem quas, nulla maxime culpa consequuntur saepe quod libero quaerat delectus ullam a, dolores nesciunt similique placeat temporibus recusandae. Accusamus.Numquam voluptatum, porro quam amet suscipit maxime fugiat alias tempora omnis mollitia voluptatibus natus laborum, temporibus totam explicabo hic, similique pariatur quasi iste assumenda. Tenetur modi ex nihil expedita laborum.Ullam animi adipisci alias dolor accusamus illo numquam dolores hic ipsam accusantium unde, natus earum cupiditate doloribus quisquam, obcaecati velit rem, deserunt voluptatibus ab reprehenderit maxime blanditiis dicta voluptates molestias.Illum nisi autem necessitatibus aperiam voluptatibus, fuga vel aliquid, ipsam beatae provident inventore voluptates, consectetur quo officiis? Animi minus assumenda eum corrupti cumque fugit eaque, at cupiditate laudantium id error!Animi officiis veritatis error officia libero possimus fugit minus, repellendus, saepe alias quasi deserunt quia exercitationem nulla delectus dicta, eaque voluptas! Sed delectus enim maxime iusto dolorum quaerat voluptatem. Cum!Est tempora, ducimus, repellendus id sed amet esse aut dolor temporibus incidunt! Praesentium eos iure facilis deleniti tempore alias nostrum nemo, distinctio voluptas similique, quibusdam natus accusantium placeat molestiae consequuntur.
+<div class="panel panel-success" style="background-color: transparent;">
+<div id="barra_age"></div>
+  <div class="panel-asd">Agenda</div>
+  <div class="panel-boddy scroll" >
+  	<table width="235" cellspacing="0" cellpadding="0" border="0">
+		<tbody>
+			<tr>
+				<td style=" background-color: white; background-repeat: repeat-x; " width="47" height="83" bgcolor="#FFFFFF">
+					<div class="dia">05</div>
+					<div class="mes">SEP</div>
+				</td>
+				<td class="age_contenido" width="188">
+					UACh conmemorará aniversario institucional en Puerto Montt
+					<a href="http://noticias.uach.cl/titulares/noticias.php?pag=noticia-externo%26cod%3D107786">Ver más</a>
+				</td>
+			</tr>
+			<tr>
+				<td style=" " width="47" height="83" bgcolor="#FFFFFF">
+					<div class="dia">05</div>
+					<div class="mes">SEP</div>
+				</td>
+				<td class="age_contenido" width="188">
+					Concierto inaugural del Ciclo de Mujeres Pianistas Inés Gebhard
+					<a href="http://noticias.uach.cl/titulares/noticias.php?pag=noticia-externo%26cod%3D107594">Ver más</a>
+				</td>
+			</tr>
+			<tr>
+				<td style=" " width="47" height="83" bgcolor="#FFFFFF">
+					<div class="dia">06</div>
+					<div class="mes">SEP</div>
+				</td>
+				<td class="age_contenido" width="188">
+					Estreno de "Johnny 100 pesos, capítulo dos" en Cine Club
+					<a href="http://noticias.uach.cl/titulares/noticias.php?pag=noticia-externo%26cod%3D107846">Ver más</a>
+				</td>
+			</tr>
+		</tbody>
+	</table>
   </div>
 </div>
 
 
+<style type="text/css">
+
+
+.mes {
+    background-color: #cccccc;
+    color: white;
+    font-size: 9px;
+    height: 15px;
+    line-height: 15px;
+    margin: 0 auto;
+    text-align: center;
+    width: 34px;
+}
+
+.dia {
+    color: #cccccc;
+    font-size: 25px;
+    height: 20px;
+    line-height: 20px;
+    margin-bottom: 3px;
+    text-align: center;
+}
+
+.age_contenido {
+    color: #666666;
+    font-size: 14px;
+    font-style: italic;
+    line-height: 16px;
+    padding-left: 18px;
+    text-align: left;
+}
+
+.panel-asd{
+	background-color: #ccc;
+    color: #fff;
+
+    font-size: 18px;
+    height: 26px;
+    line-height: 26px;
+    text-align: center;
+
+}
+
+#barra_age {
+	background-color: green;
+    height: 4px;
+}
+
+</style>

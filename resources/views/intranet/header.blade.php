467750f355f85d718da5d7b8119768ec261155d1
Hellthrash <hellthrash@gmail.com>
2018-04-10 17:26:17 -0300

    solucionado click en movilidad (intranet)

--- resources/views/intranet/header.blade.php
+++ resources/views/intranet/header.blade.php
@@ -3,7 +3,7 @@
           <div class="fa fa-bars tooltips" data-placement="right" data-original-title="Esconder menu"></div>
       </div>
     <!--logo start-->
-    <a href="index.html" class="logo"><b>MOVILIDAD</b></a>
+    <a href="home" class="logo"><b>MOVILIDAD</b></a>
     <!--logo end-->
     <div class="nav notify-row" id="top_menu">
         <!--  notification start -->
